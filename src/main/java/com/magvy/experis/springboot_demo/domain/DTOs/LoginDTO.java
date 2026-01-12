5767485c5afc70ce1cb38710633fcd4b0dfe96b0
Markus Adrian Halsvik <markus.halsvik@no.experis.com>
2026-01-12 13:05:23 +0100

    init

---
+++ src/main/java/com/magvy/experis/springboot_demo/domain/DTOs/LoginDTO.java
@@ -0,0 +1,30 @@
+package com.magvy.experis.springboot_demo.domain.DTOs;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class LoginDTO {
+    @JsonProperty("user_name")
+    private String username;
+    private String password;
+
+    public LoginDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String toString() {
+        return String.format(
+            """
+            Username: %s
+            Password: %s
+            """, this.username, this.password);
+    }
+}
