d783078b1ef8b77c5ea84fb59ad26efa4eaa5cde
Kristian Fredrik Lynum <kristian.lynum@no.experis.com>
2026-01-23 15:24:18 +0100

    creation of comment

--- src/main/java/com/magvy/experis/javalava_backend/controllers/CommentController.java
+++ src/main/java/com/magvy/experis/javalava_backend/controllers/CommentController.java
@@ -1,4 +1,23 @@
 package com.magvy.experis.javalava_backend.controllers;
 
+import com.magvy.experis.javalava_backend.application.DTOs.incomingDTO.CommentDTORequest;
+import com.magvy.experis.javalava_backend.domain.services.CommentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/comment")
 public class CommentController {
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/create")
+    public HttpStatus CreateCommentHandler(@RequestBody CommentDTORequest commentDTO) {
+        if (commentService.createPost(commentDTO)) return HttpStatus.OK;
+        else return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
 }
--- src/main/java/com/magvy/experis/javalava_backend/domain/services/CommentService.java
+++ src/main/java/com/magvy/experis/javalava_backend/domain/services/CommentService.java
@@ -1,4 +1,43 @@
 package com.magvy.experis.javalava_backend.domain.services;
 
+import com.magvy.experis.javalava_backend.application.DTOs.incomingDTO.CommentDTORequest;
+import com.magvy.experis.javalava_backend.application.DTOs.incomingDTO.PostDTORequest;
+import com.magvy.experis.javalava_backend.domain.entitites.Comment;
+import com.magvy.experis.javalava_backend.domain.entitites.Post;
+import com.magvy.experis.javalava_backend.domain.entitites.User;
+import com.magvy.experis.javalava_backend.domain.exceptions.MissingUserException;
+import com.magvy.experis.javalava_backend.infrastructure.repositories.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.sql.Date;
+import java.util.Optional;
+
+@Service
 public class CommentService {
+
+    private UserService userService;
+    private PostService postService;
+    private CommentRepository CommentRepository;
+
+    public CommentService(UserService userService, PostService postService, CommentRepository commentRepository) {
+        this.userService = userService;
+        this.postService = postService;
+        CommentRepository = commentRepository;
+    }
+
+
+    public boolean createPost(CommentDTORequest commentDTO) {
+        Comment comment = convertToEntity(commentDTO);
+        CommentRepository.save(comment);
+        return true;
+    }
+
+    public Comment convertToEntity(CommentDTORequest commentDTO) {
+        return new Comment(
+                commentDTO.getContent(),
+                Date.valueOf(commentDTO.getPublished()),
+                postService.findByID(commentDTO.getPostId()),
+                userService.findById(commentDTO.getUserId())
+        );
+    }
 }
