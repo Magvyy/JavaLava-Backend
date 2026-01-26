ce75a83cac4fee9e9471d05dfc28ee4c8122395d
Victor Larsen Dehli <victor.dehli@no.experis.com>
2026-01-26 11:44:31 +0100

    Changed to follow ConvertEntity pattern

--- src/main/java/com/magvy/experis/javalava_backend/domain/services/MessageService.java
+++ src/main/java/com/magvy/experis/javalava_backend/domain/services/MessageService.java
@@ -1,5 +1,6 @@
 package com.magvy.experis.javalava_backend.domain.services;
 
+import com.magvy.experis.javalava_backend.application.DTOs.MessageDTO;
 import com.magvy.experis.javalava_backend.domain.entitites.Friend;
 import com.magvy.experis.javalava_backend.domain.entitites.Message;
 import com.magvy.experis.javalava_backend.domain.entitites.User;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -23,32 +25,14 @@ public class MessageService {
         this.userService = userService;
     }
 
-    private boolean isFriend(User messageSender, User messageReceiver) {
-        List<Friend> friendList = friendService.getAllFriendsByUser1(messageSender);
-
-        for (Friend friend : friendList) {
-            if (friend.getUser1().equals(messageReceiver) || friend.getUser2().equals(messageReceiver)) {
-                return true;
-            }
-        }
-        return false;
+    private Message ConvertToEntity(MessageDTO messageDTO, User sender, User receiver) {
+        return new Message(messageDTO.getContent(), (Date) messageDTO.getDate(), sender, receiver);
     }
 
-    public String sendMessage(String to, String from, String content){
-        User userTo = userService.getUserByUsername(to);
-        User userFrom = userService.getUserByUsername(from);
 
-        LocalDate localDate = LocalDate.now();
-        Date sqlDate = Date.valueOf(localDate);
-
-        Message message = new Message(content,  sqlDate,  userFrom, userTo);
-
-//        if (isFriend(message.getFrom(), message.getTo())) {
-            messageRepository.save(message);
-            return message.getContent();
-//        } else {
-//            throw new IllegalArgumentException("Can only send messages to friends");
-//        }
+    public Message sendMessage(MessageDTO messageDTO, User sender, User receiver) {
+        Message message = ConvertToEntity(messageDTO, sender,receiver);
+        return messageRepository.save(message);
     }
 
     public List<Message> getMessageHistory(String to, String from){
@@ -58,9 +42,22 @@ public class MessageService {
         return messageRepository.getMessageByTo(userTo, userFrom);
     }
 
+
     public List<Message> getAllMessagesToUser(String user) {
         User recipient = userService.getUserByUsername(user);
             return messageRepository.getMessageByTo(recipient);
+    }
+
+
+    private boolean isFriend(User messageSender, User messageReceiver) {
+        List<Friend> friendList = friendService.getAllFriendsByUser1(messageSender);
+
+        for (Friend friend : friendList) {
+            if (friend.getUser1().equals(messageReceiver) || friend.getUser2().equals(messageReceiver)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
