54ce096d05ff6e34dfe350b7dbe8d126870e9987
Kristian Fredrik Lynum <kristian.lynum@no.experis.com>
2026-01-26 13:05:43 +0100

    added update and delete comment

--- src/main/java/com/magvy/experis/javalava_backend/controllers/CommentController.java
+++ src/main/java/com/magvy/experis/javalava_backend/controllers/CommentController.java
@@ -1,11 +1,13 @@
 package com.magvy.experis.javalava_backend.controllers;
 
 import com.magvy.experis.javalava_backend.application.DTOs.incomingDTO.CommentDTORequest;
+import com.magvy.experis.javalava_backend.application.DTOs.incomingDTO.PostDTORequest;
 import com.magvy.experis.javalava_backend.application.DTOs.outgoingDTO.CommentDTOResponse;
 import com.magvy.experis.javalava_backend.domain.entitites.User;
 import com.magvy.experis.javalava_backend.domain.services.CommentService;
 import com.magvy.experis.javalava_backend.infrastructure.repositories.UserRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,10 +33,22 @@ public class CommentController {
         if (commentService.createPost(commentDTO, user)) return HttpStatus.OK;
         else return HttpStatus.INTERNAL_SERVER_ERROR;
     }
-    @GetMapping("/load")
-    public List<CommentDTOResponse> LoadCommentsHandler(@RequestParam int page,@RequestParam int postId, @RequestParam (required = false) Authentication auth) {
+    @GetMapping("/{postId}")
+    public List<CommentDTOResponse> LoadCommentsHandler(@PathVariable int postId, @RequestParam int page, @RequestParam (required = false) Authentication auth) {
         User user = getLoggedInUser(auth);
         return commentService.loadCommentsByPost(page, postId, user);
+    }
+    @PutMapping("/{commentId}")
+    public ResponseEntity<CommentDTOResponse> UpdateCommentsHandler(@PathVariable int commentId, @RequestBody CommentDTORequest commentDTORequest, @RequestParam Authentication auth) {
+        User user = getLoggedInUser(auth);
+        if (user == null) return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        return commentService.edit(commentId, user, commentDTORequest);
+    }
+    @DeleteMapping("/{commentId}")
+    public HttpStatus DeleteCommentsHandler(@PathVariable int commentId, @RequestBody CommentDTORequest commentDTORequest, @RequestParam Authentication auth) {
+        User user = getLoggedInUser(auth);
+        if (user == null) return HttpStatus.UNAUTHORIZED;
+        return commentService.delete(commentId, user, commentDTORequest);
     }
 
     private User getLoggedInUser(Authentication auth) {
