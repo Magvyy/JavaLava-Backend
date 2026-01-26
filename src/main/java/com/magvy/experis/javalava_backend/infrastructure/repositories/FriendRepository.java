9724b71da611a51ef728f5f26792515e68e7b9c1
Victor Larsen Dehli <victor.dehli@no.experis.com>
2026-01-26 10:09:19 +0100

    Added getFriends for user method to repo

--- src/main/java/com/magvy/experis/javalava_backend/infrastructure/repositories/FriendRepository.java
+++ src/main/java/com/magvy/experis/javalava_backend/infrastructure/repositories/FriendRepository.java
@@ -1,10 +1,15 @@
 package com.magvy.experis.javalava_backend.infrastructure.repositories;
 
 import com.magvy.experis.javalava_backend.domain.entitites.Friend;
+import com.magvy.experis.javalava_backend.domain.entitites.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Integer> {
+
+    List<Friend> getAllFriendsByUser1(User user);
 
 }
