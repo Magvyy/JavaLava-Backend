a4f71586e7cf274b5f2ef6c740e3bb5fc42c7fdd
Victor Larsen Dehli <victor.dehli@no.experis.com>
2026-01-26 10:08:19 +0100

    Added message-getters for messagerepo

--- src/main/java/com/magvy/experis/javalava_backend/infrastructure/repositories/MessageRepository.java
+++ src/main/java/com/magvy/experis/javalava_backend/infrastructure/repositories/MessageRepository.java
@@ -1,10 +1,19 @@
 package com.magvy.experis.javalava_backend.infrastructure.repositories;
 
 import com.magvy.experis.javalava_backend.domain.entitites.Message;
+import com.magvy.experis.javalava_backend.domain.entitites.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
+
+    List<Message> getMessageByTo(User to);
+    List<Message> getMessageByFrom(User from);
+    List<Message> getMessageByTo(User to, User from);
+    List<Message> getMessageByFrom(User from, User to);
+
 
 }
