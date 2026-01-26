c04e9ea699060d6a8fb7f10f6ebe6f800461e80a
Victor Larsen Dehli <victor.dehli@no.experis.com>
2026-01-26 13:39:45 +0100

    Changed to incoming/outgoing DTO

--- src/main/java/com/magvy/experis/javalava_backend/application/DTOs/MessageDTORequest.java
+++ src/main/java/com/magvy/experis/javalava_backend/application/DTOs/MessageDTORequest.java
@@ -1,28 +1,33 @@
 package com.magvy.experis.javalava_backend.application.DTOs;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.magvy.experis.javalava_backend.domain.entitites.Message;
+import com.magvy.experis.javalava_backend.domain.entitites.User;
 import lombok.Getter;
 
 import java.sql.Date;
+
 @Getter
 public class MessageDTORequest {
+
+    @JsonProperty("message_id")
+    private int id;
+
     @JsonProperty("content")
     private String content;
-    @JsonProperty("from")
-    private String sender;
-    @JsonProperty("to")
-    private String receiver;
-    @JsonProperty("date")
+
+    @JsonProperty("sent")
     private Date date;
 
-    public MessageDTORequest(Message message) {
-        this.content = message.getContent();
-        this.sender = message.getFrom().getUsername();
-        this.receiver = message.getTo().getUsername();
-        this.date = message.getDate();
-    }
+    @JsonProperty("To")
+    private int receiver_id;
 
-    public MessageDTORequest() {
+    public MessageDTORequest() {}
+
+    public MessageDTORequest(int receiver_id, String content, Date date)
+    {
+        this.receiver_id = receiver_id;
+        this.content = content;
+        this.date = date;
+
     }
 }
