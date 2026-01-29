75aa3c1f38a7ef9ad684e419ffeda1ae14cf8793
Kristian Fredrik Lynum <kristian.lynum@no.experis.com>
2026-01-29 14:07:54 +0100

    changed repository to reflect entitiy rework

--- src/main/java/com/magvy/experis/javalava_backend/infrastructure/repositories/FriendRepository.java
+++ src/main/java/com/magvy/experis/javalava_backend/infrastructure/repositories/FriendRepository.java
@@ -2,12 +2,13 @@ package com.magvy.experis.javalava_backend.infrastructure.repositories;
 
 import com.magvy.experis.javalava_backend.domain.entitites.Friend;
 import com.magvy.experis.javalava_backend.domain.entitites.User;
+import com.magvy.experis.javalava_backend.domain.entitites.composite.FriendId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface FriendRepository extends JpaRepository<Friend, Long> {
+public interface FriendRepository extends JpaRepository<Friend, FriendId> {
     boolean existsByUser1IdAndUser2Id(Long user1Id, Long user2Id);
 }
