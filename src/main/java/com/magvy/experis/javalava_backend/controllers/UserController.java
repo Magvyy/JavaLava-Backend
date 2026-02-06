e76fc94dd7b459d89262cdb6ca4cefa77b436348
Kristian Fredrik Lynum <kristian.lynum@no.experis.com>
2026-02-06 10:26:15 +0100

    new get mapping for profiledto

--- src/main/java/com/magvy/experis/javalava_backend/controllers/UserController.java
+++ src/main/java/com/magvy/experis/javalava_backend/controllers/UserController.java
@@ -1,15 +1,20 @@
 package com.magvy.experis.javalava_backend.controllers;
 
 
+import com.magvy.experis.javalava_backend.application.DTOs.outgoing.ProfileDTOResponse;
 import com.magvy.experis.javalava_backend.application.DTOs.outgoing.UserDTOResponse;
+import com.magvy.experis.javalava_backend.application.security.config.CustomUserDetails;
+import com.magvy.experis.javalava_backend.domain.entitites.User;
 import com.magvy.experis.javalava_backend.domain.services.UserService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
-public class UserController {
+public class UserController extends BaseAuthHController {
 
     private final UserService userService;
 
@@ -25,5 +30,10 @@ public class UserController {
     @GetMapping("/{userId}")
     public UserDTOResponse getUserById(@PathVariable Long userId){
         return userService.convertToDTO(userService.getUserById(userId));
+    }
+    @GetMapping("/profile/{userId}")
+    public ProfileDTOResponse getUserById(@PathVariable Long userId, @AuthenticationPrincipal CustomUserDetails principal){
+        Optional<User> user = getUserIfAuth(principal);
+        return userService.getProfile(userId, user);
     }
 }
