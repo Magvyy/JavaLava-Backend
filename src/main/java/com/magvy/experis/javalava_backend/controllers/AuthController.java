935d49c61e8c61d3a286670d8c305d859ffe649e
Magvyy <MAHImprovement@gmail.com>
2026-02-06 10:00:05 +0100

    added the jwt to cookies

--- src/main/java/com/magvy/experis/javalava_backend/controllers/AuthController.java
+++ src/main/java/com/magvy/experis/javalava_backend/controllers/AuthController.java
@@ -4,6 +4,8 @@ import com.magvy.experis.javalava_backend.application.DTOs.incoming.AuthDTO;
 import com.magvy.experis.javalava_backend.application.security.config.CustomUserDetails;
 import com.magvy.experis.javalava_backend.application.security.filter.util.JwtUtil;
 import com.magvy.experis.javalava_backend.domain.services.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -32,29 +34,29 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity <Map<String, String>> LoginPostHandler(@RequestBody AuthDTO authDTO) {
-        return AuthHandler(authDTO);
+    public ResponseEntity <?> LoginPostHandler(@RequestBody AuthDTO authDTO, HttpServletResponse response) {
+        return AuthHandler(authDTO, response);
     }
 
     @PostMapping("/register")
-    public ResponseEntity <Map<String, String>> RegisterPostHandler(@RequestBody AuthDTO authDTO) {
-        ResponseEntity <Map<String, String>> response = userService.register(authDTO);
-        if (response.getStatusCode().equals(HttpStatusCode.valueOf(200))) {
-            return AuthHandler(authDTO);
-        }
-        return response;
+    public ResponseEntity <?> RegisterPostHandler(@RequestBody AuthDTO authDTO, HttpServletResponse response) {
+        userService.register(authDTO);
+        return AuthHandler(authDTO, response);
     }
 
-    private ResponseEntity <Map<String, String>> AuthHandler(AuthDTO authDTO) {
+    private ResponseEntity <?> AuthHandler(AuthDTO authDTO, HttpServletResponse response) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(authDTO.getUserName(), authDTO.getPassword())
         );
-        Map<String, String> response = new HashMap<>();
-        response.put("jwt", jwtUtil.generateToken(authDTO.getUserName()));
-        Object principal = authentication.getPrincipal();
-        if (principal instanceof CustomUserDetails details) {
-            response.put("user_id", String.valueOf(details.getUser().getId()));
-        }
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        String jwt = jwtUtil.generateToken(authDTO.getUserName());
+        Cookie jwtCookie = new Cookie("jwt", jwt);
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setSecure(false);
+        jwtCookie.setPath("/");
+        jwtCookie.setMaxAge(60 * 60);
+
+        response.addCookie(jwtCookie);
+        return ResponseEntity.ok("Authenticated");
     }
+
 }
