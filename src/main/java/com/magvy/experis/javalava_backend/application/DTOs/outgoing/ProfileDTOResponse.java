9cfcd57f3dbff431af8818224ebf1866387014c1
Kristian Fredrik Lynum <kristian.lynum@no.experis.com>
2026-02-06 10:23:49 +0100

    new dto for profile loading

--- src/main/java/com/magvy/experis/javalava_backend/application/DTOs/outgoing/ProfileDTOResponse.java
+++ src/main/java/com/magvy/experis/javalava_backend/application/DTOs/outgoing/ProfileDTOResponse.java
@@ -1,4 +1,22 @@
 package com.magvy.experis.javalava_backend.application.DTOs.outgoing;
 
+import com.magvy.experis.javalava_backend.domain.entitites.User;
+import com.magvy.experis.javalava_backend.domain.enums.FriendStatus;
+import lombok.Getter;
+
+@Getter
 public class ProfileDTOResponse {
+
+    private Long id;
+    private String userName;
+    private FriendStatus friendStatus;
+
+    public ProfileDTOResponse() {}
+
+    public ProfileDTOResponse(User user, FriendStatus friendStatus) {
+        this.friendStatus = friendStatus;
+        this.id = user.getId();
+        this.userName = user.getUserName();
+    }
 }
+
