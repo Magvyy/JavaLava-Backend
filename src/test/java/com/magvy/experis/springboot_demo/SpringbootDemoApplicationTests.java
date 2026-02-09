a18b45322a85bdb706ac6f42f2b51cf4b4cd665c
thobla <thorgalblanco@gmail.com>
2026-02-09 13:43:36 +0100

    Unit tests for LikeService.likePost method
    One tests that it successfully adds a like, and another to check that it gives a conflict if italready exists

--- src/test/java/com/magvy/experis/springboot_demo/SpringbootDemoApplicationTests.java
+++ src/test/java/com/magvy/experis/springboot_demo/SpringbootDemoApplicationTests.java
@@ -3,11 +3,14 @@ package com.magvy.experis.javalava_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SpringbootDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
