844623b3253aef3be454a51dc7d090e5de4c1fb9
Magvyy <MAHImprovement@gmail.com>
2026-02-11 13:15:07 +0100

    added new query to get the messages as explained in previous commit

--- src/main/java/com/magvy/experis/javalava_backend/infrastructure/repositories/MessageRepository.java
+++ src/main/java/com/magvy/experis/javalava_backend/infrastructure/repositories/MessageRepository.java
@@ -3,6 +3,7 @@ package com.magvy.experis.javalava_backend.infrastructure.repositories;
 import com.magvy.experis.javalava_backend.domain.entitites.Message;
 import com.magvy.experis.javalava_backend.domain.entitites.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,20 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
 
     List<Message> findByToAndFrom(User to, User from);
     List<Message> getMessageByFrom(User from, User to);
+
+    @Query(value = """
+    SELECT DISTINCT ON (other_user_id) *
+    FROM (
+        SELECT *,
+            CASE
+                WHEN from_user_id = :id THEN to_user_id
+                ELSE from_user_id
+            END AS other_user_id
+        FROM messages
+        WHERE from_user_id = :id OR to_user_id = :id
+    ) sub
+    ORDER BY other_user_id, sent ASC;
+    """, nativeQuery = true)
+    List<Message> getConversations(Long id);
 }
 
