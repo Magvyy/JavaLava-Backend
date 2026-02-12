e53ed84d7cda5a223e5a6a14526adc8787e4e783
Magvyy <MAHImprovement@gmail.com>
2026-02-12 08:28:30 +0100

    Made jwt cookie name an environment variable

--- src/main/java/com/magvy/experis/javalava_backend/application/security/filter/JwtFilter.java
+++ src/main/java/com/magvy/experis/javalava_backend/application/security/filter/JwtFilter.java
@@ -2,12 +2,15 @@ package com.magvy.experis.javalava_backend.application.security.filter;
 
 import com.magvy.experis.javalava_backend.application.security.filter.util.JwtUtil;
 import com.magvy.experis.javalava_backend.domain.services.UserService;
+import com.sun.tools.jconsole.JConsoleContext;
+import com.sun.tools.jconsole.JConsolePlugin;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.jspecify.annotations.NullMarked;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,11 +25,12 @@ import java.io.IOException;
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
     private final UserService userService;
-    private static final String JWT_COOKIE_NAME = "access_token";
+    private final String jwt_cookie_name;
 
-    public JwtFilter(JwtUtil jwtUtil, UserService userService) {
+    public JwtFilter(JwtUtil jwtUtil, UserService userService, @Value("${jwt.name}") String jwt_cookie_name) {
         this.jwtUtil = jwtUtil;
         this.userService = userService;
+        this.jwt_cookie_name = jwt_cookie_name;
     }
 
     @Override
@@ -54,7 +58,7 @@ public class JwtFilter extends OncePerRequestFilter {
         }
 
         for (Cookie cookie : request.getCookies()) {
-            if (JWT_COOKIE_NAME.equals(cookie.getName())) {
+            if (jwt_cookie_name.equals(cookie.getName())) {
                 return cookie.getValue();
             }
         }
