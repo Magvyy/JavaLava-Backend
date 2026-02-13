e5a7fb44431b41a8a2e084c6984b42eff2c7d17b
Magvyy <MAHImprovement@gmail.com>
2026-02-13 10:01:33 +0100

    updated exception name

--- src/main/java/com/magvy/experis/javalava_backend/controllers/BaseAuthHController.java
+++ src/main/java/com/magvy/experis/javalava_backend/controllers/BaseAuthHController.java
@@ -2,10 +2,12 @@ package com.magvy.experis.javalava_backend.controllers;
 
 import com.magvy.experis.javalava_backend.application.security.config.CustomUserDetails;
 import com.magvy.experis.javalava_backend.domain.entitites.User;
-import com.magvy.experis.javalava_backend.domain.exceptions.MissingUserException;
+import com.magvy.experis.javalava_backend.domain.exceptions.UnauthenticatedUserException;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@RestController
 public abstract class BaseAuthHController {
     private User getLoggedInUser(CustomUserDetails principal) {
         if (principal == null) return null;
@@ -15,7 +17,7 @@ public abstract class BaseAuthHController {
     protected User throwIfUserNull(CustomUserDetails principal) {
         User user = getLoggedInUser(principal);
         if (user == null) {
-            throw new MissingUserException("User not found even though authenticated");
+            throw new UnauthenticatedUserException("User is not authenticated.");
         }
         return user;
     }
