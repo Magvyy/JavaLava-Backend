ae9fc2f6825aeed942610883364e3d07dee2d97b
Magvyy <MAHImprovement@gmail.com>
2026-02-20 13:33:50 +0100

    added websocketservice dependency

--- src/test/java/com/magvy/experis/springboot_demo/serviceUnitTests/LikeServiceUnitTest.java
+++ src/test/java/com/magvy/experis/springboot_demo/serviceUnitTests/LikeServiceUnitTest.java
@@ -5,6 +5,7 @@ import com.magvy.experis.javalava_backend.domain.entitites.Like;
 import com.magvy.experis.javalava_backend.domain.entitites.Post;
 import com.magvy.experis.javalava_backend.domain.entitites.User;
 import com.magvy.experis.javalava_backend.domain.services.LikeService;
+import com.magvy.experis.javalava_backend.domain.services.WebSocketService;
 import com.magvy.experis.javalava_backend.infrastructure.repositories.LikeRepository;
 import com.magvy.experis.javalava_backend.infrastructure.repositories.PostRepository;
 import com.magvy.experis.javalava_backend.infrastructure.repositories.UserRepository;
@@ -16,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 
@@ -23,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+@ActiveProfiles("test")
 public class LikeServiceUnitTest {
 
     @Mock
@@ -33,6 +36,9 @@ public class LikeServiceUnitTest {
 
     @Mock
     PostRepository postRepository;
+
+    @Mock
+    WebSocketService webSocketService;
 
     LikeService likeService;
 
@@ -50,7 +56,7 @@ public class LikeServiceUnitTest {
     @BeforeEach
     void setUp(){
         mocks = MockitoAnnotations.openMocks(this);
-        likeService = new LikeService(likeRepository, userRepository, postRepository);
+        likeService = new LikeService(likeRepository, userRepository, postRepository, webSocketService);
     }
 
     @AfterEach
